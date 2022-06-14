a03cae299397015f060b7df6a09c55735ac3af49
Vítor Luís da Silva <82170575+Snj4y@users.noreply.github.com>
2022-06-14 10:30:07 -0300

    New a

---
+++ PrimevalAPI/Models/Repository/RepositoryContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PrimevalAPI.Models.Repository
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Repository> _repositorySet { get; set; }
+    }
+}
